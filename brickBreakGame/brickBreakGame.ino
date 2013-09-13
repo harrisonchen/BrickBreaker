ced158cf699bf8c5ed14f40e21a468b49be8c8a2
Harrison Chen <hchen030@ucr.edu>
2013-09-13 15:25:13 -0700

    Completed ball and paddle algorithms

--- brickBreakGame/brickBreakGame.ino
+++ brickBreakGame/brickBreakGame.ino
@@ -1,8 +1,14 @@
 #include <ball.h>
 #include <paddle.h>
 
-Ball ball;
+Ball ball = Ball(0x40, 0x10);
 Paddle paddle;
+int greenRowRegister[8];
+int blueRowRegister[8];
+int colRegister[8];
+int ballTimer = 0;
+int paddleTimer = 0;
+int outputTimer = 0;
 
 #include <I2Cdev.h>
 #include <MPU6050.h>
@@ -65,6 +71,51 @@ void shiftVal(int redByte, int greenByte, int blueByte, int colByte)
   digitalWrite(latchPin, HIGH);
 }
 
+void rowRegClear()
+{
+  for(int i = 0; i < 8; ++i)
+  {
+    greenRowRegister[i] = 0;
+    blueRowRegister[i] = 0;
+  }
+}
+
+void rowRegInit()
+{
+  rowRegClear();
+  for(int i = 0; i < 8; ++i)
+  {
+    if(ball.getCol() == colRegister[i])
+    {
+      blueRowRegister[i] += ball.getRow();
+    }
+    if(paddle.getLeftPaddle() == colRegister[i])
+    {
+      greenRowRegister[i] += paddle.getPaddleRow();
+    }
+    else if(paddle.getMidPaddle() == colRegister[i])
+    {
+      greenRowRegister[i] += paddle.getPaddleRow();
+    }
+    else if(paddle.getRightPaddle() == colRegister[i])
+    {
+      greenRowRegister[i] += paddle.getPaddleRow();
+    }
+  }
+}
+
+void colRegInit()
+{
+  colRegister[0] = 0x01;
+  colRegister[1] = 0x02;
+  colRegister[2] = 0x04;
+  colRegister[3] = 0x08;
+  colRegister[4] = 0x10;
+  colRegister[5] = 0x20;
+  colRegister[6] = 0x40;
+  colRegister[7] = 0x80;
+}
+
 enum ballStates{ballInit, ballUpLeft, ballUpRight, ballDownLeft, ballDownRight} ballState;
 
 void ballController()
@@ -110,14 +161,31 @@ void ballController()
     }
     case ballDownLeft:
     {
-      if( (ball.getRow() == 0x80) && (ball.getCol() == 0x01) )
-      {
-        ballState = ballUpRight;
-      }
-      else if(ball.getRow() == 0x80)
+      if( (ball.hitPaddle((paddle.getPaddleRow() >> 1), paddle.getLeftPaddle(), 
+                          paddle.getMidPaddle(), paddle.getRightPaddle())) &&
+                          (ball.getCol() == 0x01) )
+                          {
+                            ballState = ballUpRight;
+                          }
+      else if( (ball.getCol() == 0x01) && (ball.hitPaddle((paddle.getPaddleRow() >> 1), (paddle.getLeftPaddle() >> 1), 
+                          (paddle.getMidPaddle() >> 1), (paddle.getRightPaddle()) >> 1)) )
+                          {
+                            ballState = ballUpRight;
+                          }
+      else if( ball.hitPaddle((paddle.getPaddleRow() >> 1), paddle.getLeftPaddle(), 
+                          paddle.getMidPaddle(), paddle.getRightPaddle()) )
+                          {
+                            ballState = ballUpLeft;
+                          }
+      else if( ball.hitPaddle((paddle.getPaddleRow() >> 1), (paddle.getLeftPaddle() << 1), 
+                          (paddle.getMidPaddle() << 1), (paddle.getRightPaddle()) << 1) )
+                          {
+                            ballState = ballUpRight;
+                          }
+      /*else if(ball.getRow() == 0x80)
       {
         ballState = ballUpLeft;
-      }
+      }*/
       else if(ball.getCol() == 0x01)
       {
         ballState = ballDownRight;
@@ -126,14 +194,27 @@ void ballController()
     }
     case ballDownRight:
     {
-      if( (ball.getRow() == 0x80) && (ball.getCol() == 0x80) )
-      {
-        ballState = ballUpLeft;
-      }
-      else if(ball.getRow() == 0x80)
-      {
-        ballState = ballUpRight;
-      }
+      if( (ball.hitPaddle((paddle.getPaddleRow() >> 1), paddle.getLeftPaddle(), 
+                          paddle.getMidPaddle(), paddle.getRightPaddle())) &&
+                          (ball.getCol() == 0x80) )
+                          {
+                            ballState = ballUpLeft;
+                          }
+      if( (ball.getCol() == 0x80) && (ball.hitPaddle((paddle.getPaddleRow() >> 1), (paddle.getLeftPaddle() << 1), 
+                          (paddle.getMidPaddle() << 1), (paddle.getRightPaddle()) << 1)) )
+                          {
+                            ballState = ballUpLeft;
+                          }
+      else if(ball.hitPaddle((paddle.getPaddleRow() >> 1), paddle.getLeftPaddle(), 
+                          paddle.getMidPaddle(), paddle.getRightPaddle()) )
+                          {
+                            ballState = ballUpRight;
+                          }
+      else if( ball.hitPaddle((paddle.getPaddleRow() >> 1), (paddle.getLeftPaddle() >> 1), 
+                          (paddle.getMidPaddle() >> 1), (paddle.getRightPaddle()) >> 1) )
+                          {
+                            ballState = ballUpLeft;
+                          }
       else if(ball.getCol() == 0x80)
       {
         ballState = ballDownLeft;
@@ -188,20 +269,42 @@ enum paddleStates{paddleInit, paddleStay, paddleLeft, paddleRight} paddleState;
 
 void paddleController()
 {
+  accelgyro.getAcceleration(&ax, &ay, &az);
+  
   switch(paddleState)
   {
     case paddleInit:
     {
-      paddleState = paddleLeft;
+      paddleState = paddleStay;
       break;
     }
     case paddleStay:
     {
+      if(ax < 4000 && ax > -4000)
+      {
+        paddleState = paddleStay;
+      }
+      else if(ax >= 4000)
+      {
+        paddleState = paddleLeft;
+      }
+      else if(ax <= -4000)
+      {
+        paddleState = paddleRight;
+      }
       break;
     }
     case paddleLeft:
     {
-      if(paddle.getLeftPaddle() == 0x01)
+      if(ax < 4000 && ax > -4000)
+      {
+        paddleState = paddleStay;
+      }
+      else if(ax >= 4000)
+      {
+        paddleState = paddleLeft;
+      }
+      else if(ax <= -4000)
       {
         paddleState = paddleRight;
       }
@@ -209,9 +312,17 @@ void paddleController()
     }
     case paddleRight:
     {
-      if(paddle.getRightPaddle() == 0x80)
+      if(ax < 4000 && ax > -4000)
+      {
+        paddleState = paddleStay;
+      }
+      else if(ax >= 4000)
       {
         paddleState = paddleLeft;
+      }
+      else if(ax <= -4000)
+      {
+        paddleState = paddleRight;
       }
       break;
     }
@@ -249,6 +360,201 @@ void paddleController()
   }
 }
 
+enum outputStates{outputInit, col0, wait0, col1, wait1, col2, wait2, col3, wait3,
+                    col4, wait4, col5, wait5, col6, wait6, col7, wait7} outputState;
+
+void outputController()
+{
+  rowRegInit();
+  colRegInit();
+  
+  switch(outputState)
+  {
+    case outputInit:
+    {
+      outputState = col0;
+      break;
+    }
+    case col0:
+    {
+      outputState = wait0;
+      break;
+    }
+    case wait0:
+    {
+      outputState = col1;
+      break;
+    }
+    case col1:
+    {
+      outputState = wait1;
+      break;
+    }
+    case wait1:
+    {
+      outputState = col2;
+      break;
+    }
+    case col2:
+    {
+      outputState = wait2;
+      break;
+    }
+    case wait2:
+    {
+      outputState = col3;
+      break;
+    }
+    case col3:
+    {
+      outputState = wait3;
+      break;
+    }
+    case wait3:
+    {
+      outputState = col4;
+      break;
+    }
+    case col4:
+    {
+      outputState = wait4;
+      break;
+    }
+    case wait4:
+    {
+      outputState = col5;
+      break;
+    }
+    case col5:
+    {
+      outputState = wait5;
+      break;
+    }
+    case wait5:
+    {
+      outputState = col6;
+      break;
+    }
+    case col6:
+    {
+      outputState = wait6;
+      break;
+    }
+    case wait6:
+    {
+      outputState = col7;
+      break;
+    }
+    case col7:
+    {
+      outputState = wait7;
+      break;
+    }
+    case wait7:
+    {
+      outputState = col0;
+      break;
+    }
+    default:
+    {
+      outputState = outputInit;
+      break;
+    }
+  }
+  
+  switch(outputState)
+  {
+    case outputInit:
+    {
+      break;
+    }
+    case col0:
+    {
+      shiftVal(0, greenRowRegister[0], blueRowRegister[0], colRegister[0]);
+      break;
+    }
+    case wait0:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col1:
+    {
+      shiftVal(0, greenRowRegister[1], blueRowRegister[1], colRegister[1]);
+      break;
+    }
+    case wait1:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col2:
+    {
+      shiftVal(0, greenRowRegister[2], blueRowRegister[2], colRegister[2]);
+      break;
+    }
+    case wait2:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col3:
+    {
+      shiftVal(0, greenRowRegister[3], blueRowRegister[3], colRegister[3]);
+      break;
+    }
+    case wait3:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col4:
+    {
+      shiftVal(0, greenRowRegister[4], blueRowRegister[4], colRegister[4]);
+      break;
+    }
+    case wait4:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col5:
+    {
+      shiftVal(0, greenRowRegister[5], blueRowRegister[5], colRegister[5]);
+      break;
+    }
+    case wait5:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col6:
+    {
+      shiftVal(0, greenRowRegister[6], blueRowRegister[6], colRegister[6]);
+      break;
+    }
+    case wait6:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    case col7:
+    {
+      shiftVal(0, greenRowRegister[7], blueRowRegister[7], colRegister[7]);
+      break;
+    }
+    case wait7:
+    {
+      shiftVal(0xFF, 0xFF, 0xFF, 0);
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
+}
+
 void gameController()
 {
   
@@ -256,12 +562,22 @@ void gameController()
 
 void loop()
 {
-  ballController();
-  paddleController();
-  
-  shiftVal(0, paddle.getPaddleRow(), 0, paddle.getLeftPaddle());
-  shiftVal(0, paddle.getPaddleRow(), 0, paddle.getMidPaddle());
-  shiftVal(0, paddle.getPaddleRow(), 0, paddle.getRightPaddle());
-  shiftVal(0, 0, ball.getRow(), ball.getCol());
-  delay(200);
+  if(ballTimer >= 500)
+  {
+    ballController();
+    ballTimer = 0;
+  }
+  if(paddleTimer >= 250)
+  {
+    paddleController();
+    paddleTimer = 0;
+  }
+  //if(outputTimer >= 1)
+  //{
+    outputController();
+    outputTimer = 0;
+  //}
+  ++ballTimer;
+  ++paddleTimer;
+  ++outputTimer;
 }
